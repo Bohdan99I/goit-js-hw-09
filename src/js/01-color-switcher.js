aa9825ffbaace19f59ae0edde42952ce7173e7c9
Bohdan99I <bohdan.voiyov@gmail.com>
2023-06-05 19:31:29 +0300

    Task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timer = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0')}`;
+}
+
+startButton.addEventListener('click', () => {
+ timer = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', 'disabled');
+  stopButton.removeAttribute('disabled');
+});
+
+stopButton.addEventListener('click', () => {
+ startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', 'disabled');
+  clearInterval(timer);
+});
